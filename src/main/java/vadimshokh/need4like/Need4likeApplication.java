da20213554191f08b56f9b068fdf75d7ff6d28c4
Khimki-pro <vadimshoh@gmail.com>
2020-03-29 08:10:37 +0300

    Start work for my social network!

---
+++ src/main/java/vadimshokh/need4like/Need4likeApplication.java
@@ -0,0 +1,13 @@
+package vadimshokh.need4like;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Need4likeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Need4likeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vadimshokh/need4like/Need4likeApplicationTests.java
@@ -0,0 +1,13 @@
+package vadimshokh.need4like;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Need4likeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
