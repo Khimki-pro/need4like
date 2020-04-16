ae1bd5cc589b5adeecf9ab636215e825c1dd87c1
Khimki-pro <vadimshoh@gmail.com>
2020-04-16 10:05:57 +0300

    Завершил настройку горячего перезапуска (npm, Webpack), разнес компоненты в разные файлы .vue. Все работает, я просто счастливgit add --all!

--- src/main/resources/static/js/pages/App.vue
+++ src/main/resources/static/js/pages/App.vue
@@ -5,16 +5,23 @@
         </div>
         <div v-else>
             <div>{{profile.name}}&nbsp;<a href="/logout">Выйти</a></div>
-            <!--messages-list :messages="messages"/-->
+            <messages-list :messages="messages" />
         </div>
     </div>
 </template>
 
 <script>
+    import MessagesList from 'components/messages/MessageList.vue'
+
     export default {
-        data: {
-            messages: frontendData.messages,
-            profile: frontendData.profile
+        components: {
+            MessagesList
+        },
+        data() {
+            return {
+                messages: frontendData.messages,
+                profile: frontendData.profile
+            }
         }
     }
 </script>
