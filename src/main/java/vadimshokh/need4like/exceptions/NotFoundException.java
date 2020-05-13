b5e02cf88945876a972649ce8958a723a0a138fb
Khimki-pro <vadimshoh@gmail.com>
2020-05-13 08:41:24 +0300

    comment

--- src/main/java/vadimshokh/need4like/exceptions/NotFoundException.java
+++ src/main/java/vadimshokh/need4like/exceptions/NotFoundException.java
@@ -6,4 +6,4 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
 }
-//Тут должен быть комментарий.
+//Тут должен быть комментарий. Определенно.
